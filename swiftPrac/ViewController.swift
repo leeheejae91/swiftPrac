3b25fb1f5f25c78be509ce50b02566ee03fd3376
이강수 <igangsu@igangsuui-macbook-pro.local>
2016-02-02 09:43:08 +0900

    Initial Commit

---
+++ swiftPrac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftPrac
+//
+//  Created by 이강수 on 2016. 2. 2..
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
